4770678c03193936ad98e86c040691ca7ae421c3
sergei1dgb <qwertyopsok123>
2019-11-14 08:59:41 +0300

    14.11.2019 8.59

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     final static String GAMMA = "I am best of the best";
-    static String message = "I love to exercise sport and make my body stronger";
+    static String message = "I love to exercise sport and make my body strongerzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
     static char[] chars;
     public static void main(String[] args) {
         EncryptDecrypt encDec = new EncryptDecrypt();
         chars = encDec.encrypt(GAMMA, message);
        System.out.println(chars);
-       chars = encDec.dencrypt(GAMMA, chars.toString());
+       chars = encDec.dencrypt(GAMMA, String.valueOf(chars));
         System.out.println(chars);
 
 
