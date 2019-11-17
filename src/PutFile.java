139af55c55e3ec00e776abaeac9e39468f8c89b6
sergei1dgb <sergei1dgb@rambler.ru>
2019-11-17 20:57:46 +0300

    17.11.2019 20.57

--- src/PutFile.java
+++ src/PutFile.java
@@ -4,20 +4,18 @@ import java.util.ArrayList;
 public class PutFile {
 
 
-    public void putFile(ArrayList<Character> arrayList) {
+    public void putFile(ArrayList<Character> arrayList, File file) {
         OutputStreamWriter foS = null;
         try {
-
-            foS = new OutputStreamWriter(new FileOutputStream("D://123.txt"), "UTF-8");
-
-            for (int i = 0; i<arrayList.size() ; i++) {
-                foS.write(arrayList.get(i));
-
+            foS = new OutputStreamWriter(new FileOutputStream( "D://" + file.getName() ), "UTF-8");
+            for (int j = 0; j<arrayList.size() ; j++) {
+                foS.write(arrayList.get(j));
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
+                foS.flush();
                 foS.close();
             } catch (IOException e) {
                 e.printStackTrace();
--- src/TakeFile.java
+++ src/TakeFile.java
@@ -10,14 +10,22 @@ public class TakeFile {
         InputStreamReader fIS = null;
         int data=0;
          Main.message = new ArrayList<Character>();
+         File file = new File("D:");
         try {
-            fIS = new InputStreamReader(new FileInputStream("D://1.txt"), "UTF-8");
+            File[] massFiles = file.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File file, String s) {
+                    return s.endsWith("txt");
+                }
+            });
+            for(File file1: massFiles) {
+                fIS = new InputStreamReader(new FileInputStream(file1), "UTF-8");
 
-           while((data = fIS.read())!=-1){
-               Main.message.add((char)data);
-             }
-           putFile.putFile(encryptDecrypt.encrypt());
-
+                while ((data = fIS.read()) != -1) {
+                    Main.message.add((char) data);
+                }
+                putFile.putFile(encryptDecrypt.dencrypt(), file1);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }finally {
